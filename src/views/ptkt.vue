64a5acc3fbd763b80962a18347c736f58bb84de6
thekozo <99541970+thekozo@users.noreply.github.com>
2022-03-30 17:21:03 +0700

    Update ptkt.vue

--- src/views/ptkt.vue
+++ src/views/ptkt.vue
@@ -3,7 +3,7 @@
           <iframe
             :src="`https://banggia.mbs.com.vn/ptkt/`"
             width="100%"
-            height="790px"
+            height="686px"
             frameborder="0">
             </iframe>
     </div>
