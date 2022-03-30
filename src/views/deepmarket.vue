581336084751738beab80269455064fe4ffd993f
thekozo <99541970+thekozo@users.noreply.github.com>
2022-03-30 18:15:57 +0700

    Update deepmarket.vue

--- src/views/deepmarket.vue
+++ src/views/deepmarket.vue
@@ -3,7 +3,7 @@
           <iframe
             :src="`https://ifin.tvsi.com.vn/`"
             width="100%"
-            height="4516px"
+            height="4539px"
             frameborder="0">
             </iframe>
     </div>
