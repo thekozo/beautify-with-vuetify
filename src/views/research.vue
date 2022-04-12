decf6ca10233abff8a9b6a4b90a7d43a4e06d271
thekozo <99541970+thekozo@users.noreply.github.com>
2022-04-12 15:47:45 +0700

    Update research.vue

--- src/views/research.vue
+++ src/views/research.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="research">
-          <iframe
-            :src="`https://app.powerbi.com/reportEmbed?reportId=3caaba8a-bad0-4960-889f-ce2d1ef2469a&autoAuth=true&ctid=83fa3537-620a-49ba-8495-88599fe855b1&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D`"
-            width="100%"
-            height="1000px"
-            scrolling="no"
-            frameborder="0">
-            </iframe>
+          <<iframe title="Đầu Tư 101 Research Division" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNjZhNjkwZDEtZmRhOC00ZWNiLWIzZGMtMmQ2YWEwMGQyYzdmIiwidCI6IjgzZmEzNTM3LTYyMGEtNDliYS04NDk1LTg4NTk5ZmU4NTViMSIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
     </div>
 </template>
 
