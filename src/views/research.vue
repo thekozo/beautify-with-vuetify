0b61df2c922d6e22d987b9dfdbe3473124a4a45f
thekozo <99541970+thekozo@users.noreply.github.com>
2022-04-12 13:37:22 +0700

    update Dashboe

--- src/views/research.vue
+++ src/views/research.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="research">
           <iframe
-            :src="`https://onedrive.live.com/embed?cid=7C8E026AC141CDE3&resid=7C8E026AC141CDE3%21185&authkey=AAiY1rXI1gtuGP4&em=2`"
+            :src="`https://app.powerbi.com/reportEmbed?reportId=3caaba8a-bad0-4960-889f-ce2d1ef2469a&autoAuth=true&ctid=83fa3537-620a-49ba-8495-88599fe855b1&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D`"
             width="100%"
             height="1000px"
             scrolling="no"
