dee95664cb4fb2e82be2215a727fab403aa3d918
thekozo <99541970+thekozo@users.noreply.github.com>
2023-03-10 19:56:44 +0700

    Update research.vue

--- src/views/research.vue
+++ src/views/research.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="research">
-          <iframe
-            :src="`https://tradingeconomics.com/commodities`"
-            width="100%"
-            sandbox="allow-same-origin allow-scripts"
-            height="1000px"
-            scrolling="no"
-            frameborder="0">
-            </iframe>
+          <iframe src="https://example.com" sandbox="allow-same-origin allow-scripts"></iframe>
   </div>
 </template>
