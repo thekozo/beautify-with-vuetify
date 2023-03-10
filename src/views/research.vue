8462957715bd4b6f926c9eb1a4cc01109f4da900
thekozo <99541970+thekozo@users.noreply.github.com>
2023-03-10 20:24:09 +0700

    Update research.vue

--- src/views/research.vue
+++ src/views/research.vue
@@ -1,5 +1,27 @@
+</template>
+
 <template>
-  <div class="research">
-          <iframe src="https://tradingeconomics.com/commodities" sandbox="allow-same-origin allow-scripts"></iframe>
+  <div>
+    <iframe ref="myFrame" src="https://tradingeconomics.com/commodities" @load="onFrameLoad"></iframe>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onFrameLoad() {
+      // Lấy tham chiếu đến phần tử iframe
+      const myFrame = this.$refs.myFrame;
+      
+      // Lấy tham chiếu đến document bên trong iframe
+      const iframeDoc = myFrame.contentDocument || myFrame.contentWindow.document;
+      
+      // Lấy tham chiếu đến phần tử cần xóa
+      const menu = iframeDoc.getElementById('ctl00_NavigationUC1_ctl00_menu');
+      
+      // Xóa phần tử menu
+      menu.parentNode.removeChild(menu);
+    }
+  }
+};
+</script>
